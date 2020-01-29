c1a08f4686f564f32cf612e5ba592ba482838e23
rifkyar <rifkyrachman21@gmail.com>
2020-01-30 01:55:34 +0700

    Login MVC

---
+++ Login/Controllers/UserController.cs
@@ -0,0 +1,129 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BCrypt;
+
+namespace Login.Controllers
+{
+    public class UserController : Controller
+    {
+        ApplicationDbContext myContext = new ApplicationDbContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            var list = myContext.Users.ToList();
+            return View(list);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            var detail = myContext.Users.Find(id);
+            return View(detail);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var mySalt = BCrypt.Net.BCrypt.GenerateSalt();
+                user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password, mySalt);
+                myContext.Users.Add(user);
+                myContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var edit = myContext.Users.Find(id);
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, User user)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var edit = myContext.Users.Find(id);
+                edit.Email = user.Email;
+                edit.Password = user.Password;
+                myContext.Entry(edit).State = System.Data.Entity.EntityState.Modified;
+                myContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var delete = myContext.Users.Find(id);
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var delete = myContext.Users.Find(id);
+                myContext.Users.Remove(delete);
+                myContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var login = myContext.Users.Find(user.Email);
+            if (login != null && BCrypt.Net.BCrypt.Verify(user.Password, login.Password))
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Remove("id");
+            Session.Remove("Email");
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ Login/Migrations/202001291736032_TB_M_User.cs
@@ -0,0 +1,27 @@
+namespace Login.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TB_M_User : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Email = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
